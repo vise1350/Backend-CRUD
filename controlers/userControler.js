de15177cda90f7f31caf85d4b23bf843c765f530
vise1350~ <vise1350@gmail.com>
2023-07-31 11:22:34 +0530

    commit crud

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -47,69 +47,69 @@ exports.createUser=async(req,res)=>{
 
 }
 }
-// exports.getUsers=async(req,res)=>{
-//     try{
+exports.getUsers=async(req,res)=>{
+    try{
 
-//         const users=await User.find({})
-//         res.status(200).json({
-//             success:true,
-//             message:"get request successful",User
-//         })
+        const users=await User.find({})
+        res.status(200).json({
+            success:true,
+            message:"get request successful",User
+        })
 
-//     }
-//     catch{
+    }
+    catch{
 
-//         console.log(error);
-//         res.status(400).json({
-//             success:false,
-//             message:"error.message"
+        console.log(error);
+        res.status(400).json({
+            success:false,
+            message:"error.message"
 
 
-//     })
-// }
-// }
+    })
+}
+}
 
-// exports.deleteUser=async(req,res)=>{
-//     try{
+exports.deleteUser=async(req,res)=>{
+    try{
 
-//         const userId=req.params.id
-//         await User.findByIdAndDelete(userId)
-//         res.status(200).json({
-//             success:true,
-//             message:"user deleted successfully"
-//         })
+        const userId=req.params.id
+        await User.findByIdAndDelete(userId)
+        res.status(200).json({
+            success:true,
+            message:"user deleted successfully"
+        })
 
-//     }
-//     catch{
+    }
+    catch{
         
-//         console.log(error);
-//         res.status(400).json({
-//             success:false,
-//             message:"error.message"
+        console.log(error);
+        res.status(400).json({
+            success:false,
+            message:"error.message"
 
-//     })
-// }
-// }
+    })
+}
+}
 
 
-// exports.editUser=async(req,res)=>{
-//     try{
-//         const user=await User.findByIdAndUpdate(req.params.id,req.body)
-//         res.status(200).json({
-//             success:true,
-//             message:"edited successfully"
+exports.editUser=async(req,res)=>{
+    try{
+        const user=await User.findByIdAndUpdate(req.params.id,req.body)
+        res.status(200).json({
+            success:true,
+            message:"edited successfully"
         
-//         })
+        })
 
-//     }
-//     catch{
-//         console.log(error);
-//         res.status(400).json({
-//             success:false,
-//             message:"error.message"
+    }
+    catch{
+        console.log(error);
+        res.status(400).json({
+            success:false,
+            message:"error.message"
 
 
-//     })
+    })
 
-//     }
-// }
+    }
+}
